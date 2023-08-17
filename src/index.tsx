0493b6f20a86246a0ce020befe667ba9ce1c0db5
oreste-abizera <oresteabizera11@gmail.com>
2023-08-17 15:58:24 +0200

    chore: remove rfid functionality

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,6 @@ type initReader = () => Promise<boolean>;
 
 type deinitReader = () => Promise<boolean>;
 
-type isReaderInit = () => Promise<boolean>;
-
-type readSingleTag = () => Promise<any>;
-
-type readPower = () => Promise<any>;
-
-type setPower = (powerVal: number) => Promise<any>;
-
-type writeToEpc = (newStr: string) => Promise<any>;
-
 type addListener = (cb: (args: any[]) => void) => void;
 
 type removeListener = (cb: (args: any[]) => void) => void;
@@ -26,65 +16,26 @@ type startScan = () => Promise<any>;
 
 type stopScan = () => Promise<any>;
 
-type findTag = (tag: string) => Promise<any>;
-
-type clearTagCache = () => Promise<any>;
-
 const initReader: initReader = () => ChainwayC71.initReader();
 
 const deinitReader: deinitReader = () => ChainwayC71.deinitReader();
-
-const isReaderInit: isReaderInit = () => ChainwayC71.isReaderInit();
-
-const readSingleTag: readSingleTag = () => ChainwayC71.readSingleTag();
-
-const readPower: readPower = () => ChainwayC71.readPower();
-
-const setPower: setPower = (powerVal: number) => ChainwayC71.setPower(powerVal);
-
-const writeToEpc: writeToEpc = (newStr: string) =>
-  ChainwayC71.writeToEpc(newStr);
-
-const startScan: startScan = () => ChainwayC71.startScan();
-
-const stopScan: stopScan = () => ChainwayC71.stopScan();
 
 const startBarcodeScan: startScan = () => ChainwayC71.barcodeRead();
 
 const stopBarcodeScan: stopScan = () => ChainwayC71.barcodeCancel();
 
-const findTag: findTag = (tag: string) => ChainwayC71.findTag(tag);
-
-const addTagListener: addListener = (listener) =>
-  evtEmitter.addListener('UHF_TAG', listener);
-
-const removeTagListener: removeListener = (listener) =>
-  evtEmitter.removeListener('UHF_TAG', listener);
-
 const addBarcodeListener: addListener = (listener) =>
   evtEmitter.addListener('BARCODE', listener);
 
-const removeBarcodeListener: removeListener = (listener) =>
-  evtEmitter.removeListener('BARCODE', listener);
-
-const clearTagCache: clearTagCache = () => ChainwayC71.clearTags();
+const removeBarcodeListener: removeListener = (_listener) =>
+  // evtEmitter.removeListener('BARCODE', listener);
+  evtEmitter.removeAllListeners('BARCODE');
 
 export default {
   initReader,
   deinitReader,
-  isReaderInit,
-  readSingleTag,
-  readPower,
-  setPower,
-  writeToEpc,
-  startScan,
-  stopScan,
-  findTag,
-  addTagListener,
-  removeTagListener,
   addBarcodeListener,
   removeBarcodeListener,
   startBarcodeScan,
   stopBarcodeScan,
-  clearTagCache,
 };
